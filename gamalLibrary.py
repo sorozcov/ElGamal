db8d061500bbf894d3bd981b20e1e6e737ead3ad
sorozcov <42949349+sorozcov@users.noreply.github.com>
2020-10-03 21:59:32 -0600

    El Gamal

--- gamalLibrary.py
+++ gamalLibrary.py
@@ -1,20 +1,40 @@
 # Universidad del Valle de Guatemala
 # Cifrado de información 2020 2
 # Grupo 7
-# Implementation RSA.py
+# Implementation El Gamal .py
 
-import random
-from sympy import mod_inverse
-from Crypto.Util.number import bytes_to_long ,long_to_bytes
-import binascii
+#We import el gamal library
+from elgamal.elgamal import Elgamal
 
-# First Alice and Bob will communicate thru RSA
-# STEPS FOR RSA
-# 1. Generate two big random prime number p and q
-# 2. Calculate n = p*q
-# 3. Use Euler Function φ of φ(n)=(p-1)(q-1) size
-# Euler function gives all numbers between 1 and n with no common factors with n, it means are coprimes with n.
-# 4. Pick a number e between 1 and φ(n) and also e is coprime with φ(n) and n
-# 5. Calculate d knowing that e*d===1 mod φ(n)
-# e is the public key (n,e)
-# d is the private key (n,d)
+print("This is an example of el gamal cipher using elgamal python library.")
+message = b'My Secret Message'
+print("The message Alice wants to send Bob is: ", message)
+
+# Based on Taher Elgamal discrete logarithm being one way functions
+# We generate the keys needed with a big random prime number P
+# We also calculate a primitive root for P alfa.
+# We then choose a number between 1 and P, known as Lambda
+
+# Lambda will be our private key
+# The public key will be composed by P and the primitive root, being beta=alfa^lambda mod p
+publicKey, privateKey = Elgamal.newkeys(128)
+
+print("The public key will be: ", publicKey)
+print("The private key will be: ", privateKey)
+
+#We encrypt the message using the publicKey
+# To encrypt the message Alice generates a random number U
+# And uses Bob public key that has p,alfa to encrypt the message
+# Then from the operation only Bob with privateKey will be able to decrypt
+# N1 = G^U
+# N2 = M*S
+# N2*m_-1=S
+encypted = Elgamal.encrypt(message, publicKey)
+print("Encrypted Message: ", encypted)
+
+#The wen can only decrypt the message if we know the privateKey x
+# S=N1^x
+# S-1= S*N1^(q-x)
+# N2*S-1=m
+decrypted = Elgamal.decrypt(encypted, privateKey)
+print("Decrypted Message: ", decrypted)
