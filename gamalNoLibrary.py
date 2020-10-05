6e6f1955e0b6436090aa810d7cbf83714809524c
Gerardo Méndez Alvarez <36525807+gera1013@users.noreply.github.com>
2020-10-05 14:02:49 -0600

    Update gamalNoLibrary.py

--- gamalNoLibrary.py
+++ gamalNoLibrary.py
@@ -1,12 +1,12 @@
 # Universidad del Valle de Guatemala
 # Cifrado de información 2020 2
 # Grupo 7
-# Implementation El Gamal .py (sin librerias)
+# Implementación de El Gamal .py (sin librerías)
 
 import random
 import math
 
-# calcula el maximo comun denominador
+# calcula el máximo común denominador
 def gcd( a, b ):
     while b != 0:
         c = a % b
@@ -19,76 +19,74 @@ def gcd( a, b ):
 def modexp( base, exp, modulo ):
 	return pow(base, exp, modulo)
 
-class ElGamal(object):
-    def __init__(self, m):
-        # mensaje a original, cifrado y descifrado
-        self.mensaje = m
+# Alice
+# simula al emisor de un mensaje en la implementación de ElGamal
+class Alice(object):
+    def __init__(self, public_key, gpk):
         self.mensaje_cifrado = []
+
+        # obtenemos la llave pública de Bob
+        self.public_key = public_key
+
+        # se genera la llave privada de Alice (k)
+        self._private_key = gpk(self.public_key['q'])
+
+        # calculamos p - se devolverá con el mensaje cifrado
+        self.p = modexp(self.public_key['g'], self._private_key, self.public_key['q'])
+
+        print("VALORES COMPUTADOS POR ALICE")
+        print("p (g^k) calculado: ", self.p)
+        
+
+    def cipher_message(self, mensaje):
+        # calculamos s (g^ak) - s se mantiene secreto ya que Bob puede calcular s con p y su llave secreta
+        s = modexp(self.public_key['h'], self._private_key, self.public_key['q'])
+
+        for i in range(0, len(mensaje)):
+            self.mensaje_cifrado.append(ord(mensaje[i]) * s)
+
+        print("s (g^ak) calculado: ", s)
+
+        return {
+            'cifrado': self.mensaje_cifrado, 
+            'p': self.p
+        }
+
+# Bob
+# simula al receptor de un mensaje en la implementación de ElGamal
+class Bob(object):
+    def __init__(self):
+        # mensaje descifrado
         self.mensaje_descifrado = []
-        print("\nMensaje original: %s\n" % (m))
 
-        # primer paso - Bob genera una llave pública y una llave privada
-        # la llave privada se almacena para descifrar el mensaje entrante
-        self.generate_bob()
-
-        # segundo paso - Alice realiza la encripción del mensaje con la llave pública
-        self.generate_alice()
-
-    def restart(self, m):
-        self.__init__(m)
-
-    # generamos q, g, a y h
-    def generate_bob(self):
-        # se generan q y g, dos numeros grandes a ser usados en la encripcion
+    def generate_public_key(self, gpk):
+        # se generan q y g, dos numeros grandes a ser usados en el cifrado
         self.q = random.randint(pow(10, 100), pow(10, 150))
         self.g = random.randint(2, self.q)
         print("LLAVE PÚBLICA DE BOB")
         print("q calculado: ", self.q)
-        print("g calculado: ", self.g)
+        print("g elegido: ", self.g)
 
         # se genera la llave privada de Bob (a)
-        self.bob_private_key = self.generate_private_key()
+        self._private_key = gpk(self.q)
 
         # calculamos h (g^a)
-        self.h = modexp(self.g, self.bob_private_key, self.q)
+        self.h = modexp(self.g, self._private_key, self.q)
         print("h (g^a) calculado: %i\n" % (self.h))
 
-    # se genera la llave privada de alice y se computan s y p. p se publica, s se mantiene secreto
-    def generate_alice(self):
-        # se genera la llave privada de Alice (k)
-        self.alice_private_key = self.generate_private_key()
+        return {
+            'q': self.q,
+            'g': self.g,
+            'h': self.h
+        }
 
-        # calculamos s y p - s se mantiene secreto, p se devuelve con el mensaje cifrado
-        s = modexp(self.h, self.alice_private_key, self.q)
-        self.p = modexp(self.g, self.alice_private_key, self.q)
-        print("VALORES COMPUTADOS POR ALICE")
-        print("p (g^k) calculado: ", self.p)
-        print("s (g^ak) calculado: ", s)
-        
-        # ciframos el mensaje utilizando s
-        self.cifrar(s)
-    
-    # se genera la llave privada 
-    def generate_private_key(self):
-        key = random.randint(pow(10, 120), self.q)
-        while gcd(self.q, key) != 1: 
-            key = random.randint(pow(10, 120), self.q)
-        
-        return key
-
-    # método para cifrado del mensaje
-    def cifrar(self, s):
-        for i in range(0, len(self.mensaje)):
-            self.mensaje_cifrado.append(ord(self.mensaje[i]) * s)
-
-    # método para descifrar el mensaje
-    def descifrar(self):
+    def decipher_message(self, mensaje_cifrado, p):
         # calculamos s nuevamente, esta vez utilizando p y a -> p^a = (g^k)^a = g^ak = s
-        s = modexp(self.p, self.bob_private_key, self.q)
+        s = modexp(p, self._private_key, self.q)
 
-        # se descifra el mensaje dividiendolo por el valor s
-        for i in range(0, len(self.mensaje_cifrado)):
-            self.mensaje_descifrado.append(chr(int(self.mensaje_cifrado[i]/s)))
+        # se descifra el mensaje dividiéndolo por el valor s
+        for i in range(0, len(mensaje_cifrado)):
+            self.mensaje_descifrado.append(chr(int(mensaje_cifrado[i]/s)))
 
         # convertimos el mensaje descifrado en un único string 
         self.mensaje_descifrado = ''.join(self.mensaje_descifrado)
@@ -96,14 +94,60 @@ class ElGamal(object):
         # devolvemos el mensaje descifrado
         return self.mensaje_descifrado
 
+# ElGamal
+# simple implementación de un intercambio de mensajes por medio de la construcción ElGamal
+# 
+# Se recibe un mensaje de texto simple
+# Se crea una instancia de Bob (receptor) y se genera la llave pública y privada
+# Luego Alice utiliza esta llave pública para calcular p y s - s servirá para cifrar el mensaje
+# Alice envía el mensaje cifrado y el valor p a Bob para que este lo descifre
+# Bob calcula s con p y a 
+# Bob descifra el mensaje utilizando s y puede leer el texto simple
+class ElGamal(object):
+    def __init__(self, m):
+        # mensaje a original, cifrado y descifrado
+        self.mensaje = m
+        self.mensaje_cifrado = None
+        self.mensaje_descifrado = []
+        print("\nMensaje original: %s\n" % (m))
+
+        # objeto Bob (receiver)
+        self.bob = Bob()
+
+        # almacenamos la llave pública
+        self.public_key = self.bob.generate_public_key(self.generate_private_key)
+
+        # objeto Alice (sender)
+        self.alice = Alice(self.public_key, self.generate_private_key)
+
+    
+    # se genera la llave privada 
+    def generate_private_key(self, q):
+        key = random.randint(pow(10, 120), q)
+        while gcd(q, key) != 1: 
+            key = random.randint(pow(10, 120), q)
+        
+        return key
+
+    # método para cifrado del mensaje
+    def cifrar(self):
+        self.mensaje_cifrado = self.alice.cipher_message(self.mensaje)
+
+    # método para descifrar el mensaje
+    def descifrar(self):
+        self.mensaje_descifrado = self.bob.decipher_message(self.mensaje_cifrado['cifrado'], self.mensaje_cifrado['p'])
+        
+        return self.mensaje_descifrado
 
 # 
 # Llamamos a la clase e implementamos
 #
 
-mensaje = "Mi mensaje secreto"
+mensaje = "Mensaje para cifrar con ElGamal"
 
 elgamal = ElGamal(mensaje)
 
+mensaje_cifrado = elgamal.cifrar()
 mensaje_descifrado = elgamal.descifrar()
+
 print("\nMensaje descifrado: %s\n" % (mensaje_descifrado))
